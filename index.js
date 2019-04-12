73d39742fbda4aea673caab0b17229a47c97e0b2
abhinavbahl97 <abhinavbahl97@gmail.com>
2019-04-12 01:57:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -245,6 +245,6 @@ app.get('/professor_form', function(req, res){
 });
 
 
-app.listen(3000, function() {
-    console.log('listening on port 3000!');
+app.listen(process.env.PORT || 3000, function() {
+    console.log('Listening!');
 });
